88624f7fff637aacc4797824d034d170eee67a5e
daoducminh <daoducminh1997@gmail.com>
2022-07-19 21:15:10 +0700

    Initial commit

---
+++ src/main/java/com/example/demodatasource/DemoDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDataSourceApplication.class, args);
+    }
+
+}
