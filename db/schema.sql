6af00edd0f2be5db55be75b4eacad2fd0f641a39
daoducminh <daoducminh1997@gmail.com>
2022-07-21 20:32:28 +0700

    Add created_at and updated_at

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,14 @@
+CREATE
+OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at
+= NOW();
+RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
 CREATE TABLE user_info
 (
     id           bigserial primary key,
@@ -8,8 +19,16 @@ CREATE TABLE user_info
     birthday     date,
     phone_number varchar,
     address      text,
-    city         varchar
+    city         varchar,
+    created_at   timestamptz NOT NULL DEFAULT now(),
+    updated_at   timestamptz
 );
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE
+    ON user_info
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_timestamp();
 
 CREATE TABLE user_military_service
 (
@@ -20,19 +39,36 @@ CREATE TABLE user_military_service
     birthday   date,
     has_served boolean,
     year_start int,
-    year_end   int
+    year_end   int,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz
 );
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE
+    ON user_military_service
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_timestamp();
 
 CREATE TABLE user_health
 (
-    id        bigserial primary key,
-    id_number varchar,
-    name      varchar,
-    gender    boolean,
-    birthday  date,
-    height    numeric,
-    weight    numeric
+    id         bigserial primary key,
+    id_number  varchar,
+    name       varchar,
+    gender     boolean,
+    birthday   date,
+    height     numeric,
+    weight     numeric,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz
 );
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE
+    ON user_health
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_timestamp();
+
 
 CREATE TABLE user_education
 (
@@ -45,5 +81,13 @@ CREATE TABLE user_education
     primary_school   varchar,
     secondary_school varchar,
     high_school      varchar,
-    college          varchar
+    college          varchar,
+    created_at       timestamptz NOT NULL DEFAULT now(),
+    updated_at       timestamptz
 );
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE
+    ON user_education
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_timestamp();
